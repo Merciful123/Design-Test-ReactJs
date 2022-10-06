655674bc20d1a6ce4c9423579deab59faf880900
Aamir Raza <merciful9795@gmail.com>
2022-10-07 00:53:29 +0530

    All done but default size of image 3 couldn't done, tried it but loosed features in amination.

--- client/src/App.js
+++ client/src/App.js
@@ -6,29 +6,44 @@ import kid3 from "./images/kid3.jpg";
 import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
-    <div class="main">
+    <div className="main">
       {/* <!-- This main class is for aligning children into column --> */}
       <h2>The childhood memories</h2>
-      <div class="row">
+      <div className="row">
         {/* <!-- Below 3 divs are to align them in row --> */}
 
-        <div class="image">
-          {/* <!-- The image class is for text position --> */}
-          <div class="text">STRENGTH</div>
-          <FontAwesomeIcon className="fa-solid" icon={faCircleChevronRight} />
-          <img class="img1" src={kid1} alt="" />
+        <div className="image">
+          {/* <!-- The image className is for text position --> */}
+          <div className="image-item">STRENGTH</div>
+          <div>
+            <FontAwesomeIcon className="fa-solid" icon={faCircleChevronRight} />
+          </div>
+          <div>
+            <img className="img1 " src={kid1} alt="" />
+          </div>
         </div>
 
-        <div class="image">
-          <div class="text">MOBILITY</div>
-          <FontAwesomeIcon className="fa-solid" icon={faCircleChevronRight} />
-          <img src={kid2} alt="" />
+        <div className="image">
+          <div className="image-item">MOBILITY</div>
+          <div>
+            <FontAwesomeIcon className="fa-solid" icon={faCircleChevronRight} />
+          </div>
+          <div>
+            <img className="img2" src={kid2} alt="" />
+          </div>
         </div>
-        <div class="image">
-          <div lass="text">DRILLS</div>
-          <FontAwesomeIcon className="fa-solid" icon={faCircleChevronRight} />
-          {/* <i class="fa-solid fa-circle-chevron-right"></i> */}
-          <img class="img3" src={kid3} alt="" />
+        <div className="image"  >
+          <div className="image-item">DRILLS</div>
+          <div>
+            <FontAwesomeIcon
+              className="fa-solid "
+              icon={faCircleChevronRight}
+            />
+          </div>
+          <div  >
+            <img className="img3"  src={kid3} alt="" />
+          </div>
+          {/* <i className="fa-solid fa-circle-chevron-right"></i> */}
         </div>
       </div>
     </div>
