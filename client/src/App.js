e7e225c7c70b2eef8fbde52967ff030a4f0f6837
Aamir Raza <merciful9795@gmail.com>
2022-10-07 10:14:23 +0530

    Fixed the error, Now it is the exact clone of the sample

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,25 @@ import kid3 from "./images/kid3.jpg";
 import { faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
-    <div className="main">
-      {/* <!-- This main class is for aligning children into column --> */}
+    <div className="container">
+      {/* <!-- This main class is for aligning children into columns --> */}
       <h2>The childhood memories</h2>
       <div className="row">
-        {/* <!-- Below 3 divs are to align them in row --> */}
+        {/* <!-- Below 3 divs are to align them in rows --> */}
 
         <div className="image">
-          {/* <!-- The image className is for text position --> */}
-          <div className="image-item">STRENGTH</div>
+          {/* <!-- The image-item className is for text position --> */}
+          <div className="image-text">STRENGTH</div>
           <div>
             <FontAwesomeIcon className="fa-solid" icon={faCircleChevronRight} />
           </div>
-          <div>
+          <div id="pic1">
             <img className="img1 " src={kid1} alt="" />
           </div>
         </div>
 
         <div className="image">
-          <div className="image-item">MOBILITY</div>
+          <div className="image-text">MOBILITY</div>
           <div>
             <FontAwesomeIcon className="fa-solid" icon={faCircleChevronRight} />
           </div>
@@ -32,8 +32,8 @@ function App() {
             <img className="img2" src={kid2} alt="" />
           </div>
         </div>
-        <div className="image"  >
-          <div className="image-item">DRILLS</div>
+        <div className="image" id="div3" >
+          <div className="image-text">DRILLS</div>
           <div>
             <FontAwesomeIcon
               className="fa-solid "
